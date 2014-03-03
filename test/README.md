66664eb9034e517f21e3c25c8a41db756a94dbd7
frantic1048 <franticblack@163.com>
2014-03-03 17:49:28 +0800

    adding a test folder

---
+++ test/README.md
@@ -0,0 +1,2 @@
+This is just a testing folder for web preview,
+will be removed when the page is full completed. 
---
+++ test/view_pannel.js
@@ -0,0 +1,4 @@
+function click_preview(el){
+    view = document.getElementById("view_display")
+    view.setAttribute("src" ,el.src);
+}
