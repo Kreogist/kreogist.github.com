6df41592b38837959c6720ccadb4f31f0a426a79
frantic1048 <franticblack@163.com>
2014-09-05 23:02:43 +0800

    aloha ~

--- javascripts/control.js
+++ javascripts/control.js
@@ -1,28 +1,51 @@
-var large_view_pic="large-view-pic";
+var large_view_pic="large-view-";
 var small_view_pic="small-view-pic-";
 var summary="summary-";
-var sm_count=4;
+var sm_count=2;
+
+function toggleActive(){
+    for(var i = 0;i < arguments.length;++i){
+        el = arguments[i];
+        if(el.classList.contains('active')){
+            el.classList.add('active');
+        }
+        else{
+            el.classList.remove('active');
+        }
+    }
+    return 0;
+}
+
+function setActive(){
+    for(var i = 0;i < arguments.length;++i){
+        el = arguments[i];
+        if(!(el.classList.contains('active'))){
+            el.classList.add('active');
+        }
+    }
+    return 0;
+}
+
+function unsetActive(){
+    for(var i = 0;i < arguments.length;++i){
+        el = arguments[i];
+        if(el.classList.contains('active')){
+            el.classList.remove('active');
+        }
+    }
+    return 0;
+}
 
 function click_preview(view_pic){
     idx=view_pic.getAttribute("index").toString();
     for(var i = 1;i <= sm_count;++i){
         var el_summary = document.getElementById(summary + i.toString());
-        var el_small_view = document.getElementById(small_view_pic + i.toString()).parentElement;
+        var el_small_view_pic = document.getElementById(small_view_pic + i.toString()).parentElement;
+        var el_large_view_pic = document.getElementById(large_view_pic + i.toString());
         if(i.toString()==idx){
-            if( !(el_summary.classList.contains('active')) ){
-                el_summary.classList.add('active');
-            }
-            if( !(el_small_view.classList.contains('active')) ){
-                el_small_view.classList.add('active');
-            }
+            setActive(el_summary,el_small_view_pic,el_large_view_pic);
             continue;
         }
-        if ( el_summary.classList.contains('active') ){
-            el_summary.classList.remove('active');
-        }
-        if(el_small_view.classList.contains('active')){
-            el_small_view.classList.remove('active');
-        }
+        unsetActive(el_summary,el_small_view_pic,el_large_view_pic);
     }
-    document.getElementById(large_view_pic).setAttribute("src",document.getElementById(small_view_pic+idx).src);
 }
