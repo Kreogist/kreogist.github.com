ec15a8cf62eb553e2552d1ead504383403470c17
Saki <tomguts@126.com>
2017-07-25 13:13:21 +1000

    Update the new URL for Mac OS X platform.

--- Mu/javascripts/kdownloadlinks.js
+++ Mu/javascripts/kdownloadlinks.js
@@ -26,7 +26,7 @@ var kDownloadLinks = [
     ['', '', 'https://pan.baidu.com/s/1bV7iUE' , 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta3/Windows/mu_1.0.0-beta3_win64_intel_fd1fea5.7z/download'],
     ['', '', 'https://pan.baidu.com/s/1slbirEl', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta3/Windows/mu_1.0.0-beta3_win64_amd_fd1fea5.zip/download'],
     ['', '', 'https://pan.baidu.com/s/1nvaAx41', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta3/Windows/mu_1.0.0-beta3_win64_amd_fd1fea5.7z/download'],
-    ['', '', 'https://pan.baidu.com/s/1pLhXUNh', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta3/Mac/mu_1.0.0-beta3_mac_fd1fea5.dmg/download'],
-    ['', '', 'https://pan.baidu.com/s/1bp7yQf1', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta3/Mac/mu_1.0.0-beta3_mac_fd1fea5.app.zip/download'],
+    ['', '', 'https://pan.baidu.com/s/1o8uRrYi', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta3/Mac/mu_1.0.0-beta3_mac_fd1fea5.dmg/download'],
+    ['', '', 'https://pan.baidu.com/s/1qYobAFi', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta3/Mac/mu_1.0.0-beta3_mac_fd1fea5.app.zip/download'],
     ['', '', 'https://pan.baidu.com/s/1eRVzccE', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta3/Linux/mu_1.0.0-beta2_ubuntu_amd64_fd1fea5.deb/download']
 ];
