1ad6b235d47e611f763c83d777df2aac3e0a4c65
Saki <tomguts@126.com>
2017-11-25 04:07:47 +0800

    Add beta 5 download link

--- Mu/javascripts/kdownloadlinks.js
+++ Mu/javascripts/kdownloadlinks.js
@@ -18,15 +18,15 @@ var kAvailablePackageType = [
 ];
 
 var kDownloadLinks = [
-    ['', '', 'https://pan.baidu.com/s/1dET0hyD', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta4/Windows/mu_1.0.0-beta4_win32_0708ffe.zip/download'],
-    ['', '', 'https://pan.baidu.com/s/1bQiyPS', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta4/Windows/mu_1.0.0-beta4_win32_0708ffe.7z/download'],
-    ['', '', 'https://pan.baidu.com/s/1crSH6e', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta4/Windows/mu_1.0.0-beta4_win64_0708ffe.zip/download'],
-    ['', '', 'https://pan.baidu.com/s/1jHHeH9O', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta4/Windows/mu_1.0.0-beta4_win64_0708ffe.7z/download'],
-    ['', '', 'https://pan.baidu.com/s/1i53PExJ', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta4/Windows/mu_1.0.0-beta4_win64_intel_0708ffe.zip/download'],
-    ['', '', 'https://pan.baidu.com/s/1geXzM3T', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta4/Windows/mu_1.0.0-beta4_win64_intel_0708ffe.7z/download'],
-    ['', '', 'https://pan.baidu.com/s/1b7R7oa', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta4/Windows/mu_1.0.0-beta4_win64_amd_0708ffe.zip/download'],
-    ['', '', 'https://pan.baidu.com/s/1qXOZUQK', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta4/Windows/mu_1.0.0-beta4_win64_amd_0708ffe.7z/download'],
-    ['', '', 'https://pan.baidu.com/s/1dE0ZQeP', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta4/Mac/mu_1.0.0-beta4_mac_0708ffe.dmg/download'],
-    ['', '', 'https://pan.baidu.com/s/1qYbleOk', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta4/Mac/mu_1.0.0-beta4_mac_0708ffe.app.zip/download'],
-    ['', '', 'https://pan.baidu.com/s/1o7Xl6J4', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta4/Linux/mu_1.0.0-beta4_ubuntu_amd64_0708ffe.deb/download']
+    ['', '', 'https://pan.baidu.com/s/1i4DPdYl', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta5/Windows/mu_1.0.0-beta5_win32_dde5ccd.zip/download'],
+    ['', '', 'https://pan.baidu.com/s/1eRP071g', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta5/Windows/mu_1.0.0-beta5_win32_dde5ccd.7z/download'],
+    ['', '', 'https://pan.baidu.com/s/1mhHMJMC', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta5/Windows/mu_1.0.0-beta5_win64_dde5ccd.zip/download'],
+    ['', '', 'https://pan.baidu.com/s/1dE1LSXj', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta5/Windows/mu_1.0.0-beta5_win64_dde5ccd.7z/download'],
+    ['', '', 'https://pan.baidu.com/s/1cnVriQ', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta5/Windows/mu_1.0.0-beta5_win64_intel_dde5ccd.zip/download'],
+    ['', '', 'https://pan.baidu.com/s/1bpFD2Lh', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta5/Windows/mu_1.0.0-beta5_win64_intel_dde5ccd.7z/download'],
+    ['', '', 'https://pan.baidu.com/s/1pLqm6Pd', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta5/Windows/mu_1.0.0-beta5_win64_amd_dde5ccd.zip/download'],
+    ['', '', 'https://pan.baidu.com/s/1pKHaE9P', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta5/Windows/mu_1.0.0-beta5_win64_amd_dde5ccd.7z/download'],
+    ['', '', 'https://pan.baidu.com/s/1nuNkvaT', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta5/Mac/mu_1.0.0-beta5_mac_dde5ccd.dmg/download'],
+    ['', '', 'https://pan.baidu.com/s/1pL9juOF', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta5/Mac/mu_1.0.0-beta5_mac_dde5ccd.app.zip/download'],
+    ['', '', 'https://pan.baidu.com/s/1dEWolbj', 'https://sourceforge.net/projects/kreogist-mu/files/Releases/1.0-beta5/Linux/mu_1.0.0-beta5_ubuntu_amd64_dde5ccd.deb/download']
 ];
